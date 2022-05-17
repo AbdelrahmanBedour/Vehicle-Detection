51dc20ad9ddd04b1029a4594ca1d88d485fd2865
Abdelrahman Mohamed Salah <67156865+AbdelrahmanBedour@users.noreply.github.com>
2022-05-17 19:56:03 +0200

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -64,3 +64,87 @@ class FeaturesParameters():
         # Histogram parameters
         self.hist_bins = 32
         self.hist_range = (0, 256)
+
+def extract_features(image, params):
+    # Parameters extraction
+    # HOG parameters
+    cspace = params.cspace
+    orient = params.orient
+    pix_per_cell = params.pix_per_cell
+    cell_per_block = params.cell_per_block
+    hog_channel = params.hog_channel
+    # Spatial parameters
+    size = params.size
+    # Histogram parameters
+    hist_bins = params.hist_bins
+    hist_range = params.hist_range
+
+    # apply color conversion if other than 'RGB'
+    if cspace != 'RGB':
+        if cspace == 'HSV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif cspace == 'LUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+        elif cspace == 'HLS':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        elif cspace == 'YUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        elif cspace == 'YCrCb':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    else:
+        feature_image = np.copy(image)
+
+    # Call get_hog_features() with vis=False, feature_vec=True
+    if hog_channel == 'ALL':
+        hog_features = []
+        for channel in range(feature_image.shape[2]):
+            hog_features.append(get_hog_features(feature_image[:, :, channel],
+                                                 orient, pix_per_cell, cell_per_block,
+                                                 feature_vec=True))
+        hog_features = np.ravel(hog_features)
+    else:
+        hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                        pix_per_cell, cell_per_block, feature_vec=True)
+
+    # Apply bin_spatial() to get spatial color features
+    spatial_features = cv2.resize(feature_image, size).ravel()
+
+    # Apply color_hist()
+    hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
+
+    return np.concatenate((spatial_features, hist_features, hog_features))
+
+
+def fitModel(positive, negative, svc, scaler, params):
+    """
+    Trains the classifier `svc`. The feature extraction is done using the parameters stored in `params`.
+    The feature scalling is done by the scaler `scaler`.
+    Returns: (`svc`, fittingTime, accuracy)
+    """
+    positive_features = list(map(lambda img: extract_features(img, params), positive))
+    negatice_features = list(map(lambda img: extract_features(img, params), negative))
+
+    # Stacking and scaling
+    X = np.vstack((positive_features, negatice_features)).astype(np.float64)
+    X_scaler = scaler.fit(X)
+    scaled_X = X_scaler.transform(X)
+
+    # Defining objective
+    y = np.hstack((np.ones(len(positive_features)), np.zeros(len(negatice_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    # Fitting
+    print('model Training...')
+    t = time.time()
+    svc.fit(scaled_X, y)
+    t2 = time.time()
+
+    fittingTime = round(t2 - t, 2)
+    accuracy = round(svc.score(X_test, y_test), 4)
+    return (svc, X_scaler, fittingTime, accuracy)
+
+
+        
